7e07ba5c365b4379ee9fae4843881748c5126a68
Stefanos <stefanosu422@gmail.com>
2020-11-19 18:04:48 -0500

    added loader and message components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,7 +10,8 @@ import Product from '../models/productModel.js'
 router.get('/', asyncHandler(async (req, res) => {
   const products = await Product.find({})
   res.json(products)
-})) 
+  })
+) 
 
 // @desc Fetch single product
 // GET /api/product/:id 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 
 const HomeScreen = () => {  
@@ -21,7 +23,7 @@ useEffect(() => {
   return (
     <>
       <h1> Latest Products</h1>
-      { loading ? <h2> loading...</h2> : error ? <h3>{error}</h3> : <Row>
+      { loading ? <Loader/>: error ? <Message variant='danger'>{error}</Message> : <Row>
         {products.map(product => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product}/>

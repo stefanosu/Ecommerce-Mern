02049a964fed15a1fbf4e21776a44625cef2e47d
Stefanos <stefanosu422@gmail.com>
2020-11-23 11:16:13 -0500

    added products controller and took controller login out of productRoutes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'  
+
+
+// @desc Fetch All products 
+// GET /api/proucts
+// @access Public 
+
+const getProducts = asyncHandler(async(req, res) => {
+  const products = await Product.find({})
+
+  res.json(products)
+})
+
+
+// @desc Fetch single product
+// GET /api/product/:id 
+// @access Public  
+
+const getProductById = asyncHandler(async(req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found')  
+    }
+  })
+
+  export {getProducts, getProductById}

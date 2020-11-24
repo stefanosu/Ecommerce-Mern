66117b7677dde9204cfa6b09ddb762f6bc4ee79b
Stefanos <stefanosu422@gmail.com>
2020-11-24 16:43:04 -0500

    updated userProfile endpoint and added register screen

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,4 +1,10 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGOUT } from '../constants/userConstants' 
+import { 
+  USER_LOGIN_FAIL, 
+  USER_LOGIN_REQUEST, 
+  USER_LOGOUT, 
+  USER_REGISTER_FAIL, 
+  USER_REGISTER_REQUEST 
+} from '../constants/userConstants' 
 import axios from 'axios'
 
 
@@ -36,4 +42,42 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({type: USER_LOGOUT})
+}
+
+
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST
+    })
+    const configObj = {
+      headers: {
+        'Content-Type': 'application/json', 
+      }
+    } 
+    const { data } =  await axios.post(
+      '/api/users',
+      { name, email, password }, 
+      configObj
+      )
+      dispatch({
+        type: USER_REGISTER_REQUEST, 
+        payload: data  
+      })
+
+      dispatch({
+        type: USER_LOGIN_REQUEST, 
+        payload: data  
+      })
+
+      localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload: error.response && error.response.message 
+      ? error.response.data.message 
+      : error.message
+    })
+  }
 }

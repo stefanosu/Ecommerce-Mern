9599ffa6d448e15a6d7171e546852f2a1c817593
Stefanos <stefanosu422@gmail.com>
2020-12-11 11:14:26 -0500

    added multer for uploading files to server

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import {notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+import path from 'path'
 
 
 dotenv.config()
@@ -19,12 +22,16 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
 
 app.get('/api/config/paypal',(req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-app.get('/', (req, res) => {
-  res.send('api is running.........')
-})
+// using path.resolve to mimic it because using es6 modules 
+const __dirname = path.resolve()
+// making upload folder static so it can be loaded in the browser 
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) // <-- only available using commonJS require 
+
 
 //error handling 
 app.use(notFound) 

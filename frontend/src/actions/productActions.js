3ad0c229c4b744730c174dbe972d5494e62c5035
Stefanos <stefanosu422@gmail.com>
2020-12-17 17:12:32 -0500

    added product reviews

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,7 +14,10 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_FAIL,
-  PRODUCT_UPDATE_SUCCESS
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST
 } from '../constants/productConstants' 
 
 
@@ -148,6 +151,35 @@ export const updateProduct = (product) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_UPDATE_FAIL,
+      payload: error.response && error.response.message 
+      ? error.response.data.message 
+      : error.message
+    })
+  }
+}
+
+
+
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_CREATE_REVIEW_REQUEST })
+
+    const { userLogin: { userInfo }, } = getState()
+
+    const configObj = {
+      headers: {
+        'Content-Type': 'application/json', 
+        Authorization: `Bearer ${userInfo.token}`, 
+      },
+    } 
+    await axios.post(`/api/products/${productId}/reviews`, review, configObj)
+
+    dispatch({ type: PRODUCT_CREATE_REVIEW_SUCCESS })
+
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_FAIL,
       payload: error.response && error.response.message 
       ? error.response.data.message 
       : error.message
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux' 
 import thunk from 'redux-thunk' 
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productCreateReducer, productUpdateReducer, productDeleteReducer, productDetailsReducer, productListReducer } from './reducers/productReducers'
+import { productCreateReducer, productUpdateReducer, productDeleteReducer, productDetailsReducer, productListReducer, productCreateReviewReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer, orderListReducer, orderDeliverReducer } from './reducers/orderReducers'
@@ -12,6 +12,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer, 
   productUpdate: productUpdateReducer, 
+  productReviewCreate: productCreateReviewReducer,
   cart: cartReducer,
   userLogin: userLoginReducer, 
   userRegister: userRegisterReducer,

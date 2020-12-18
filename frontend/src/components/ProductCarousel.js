8061e2d886635efd71efe7e9e12170cb074c83f4
Stefanos <stefanosu422@gmail.com>
2020-12-18 17:22:51 -0500

    added products carousel and styling

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Carousel, Image } from 'react-router-bootstrap'
+import { Carousel, Image } from 'react-bootstrap'
 import Loader from './Loader'
 import Message from './Message'
 import { listTopProducts } from '../actions/productActions'
